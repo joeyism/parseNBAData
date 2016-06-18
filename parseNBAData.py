3fcdb3f93dfdbb33dfe3cfccc5225c2aa0d14cb2
joeyism <sham.joey@gmail.com>
2016-06-18 01:03:04 -0400

    added input args

--- parseNBAData.py
+++ parseNBAData.py
@@ -15,24 +15,27 @@ def get_sec(s):
     l = s.split(':')
     return int(l[0]) * 3600 + int(l[1]) * 60 + int(l[2])
 
-textData = []
-parsedData = {}
-with open('./data.tsv', 'rb') as tsvin:
-    tsvin = csv.reader(tsvin, delimiter = "\t")
-    currentGameId = ""
-    for i, line in enumerate(tsvin):
-        if i > 0:
-            timeInSeconds = 48 * 60 - get_sec(line[2])
-            dataLine = line[2:4]
-            dataLine[0] = timeInSeconds
-            if currentGameId == "":
-                currentGameId = line[0]
-                parsedData[currentGameId] = GameData(currentGameId, [dataLine])
-            elif currentGameId == line[0]:
-                parsedData[currentGameId].append(dataLine)
-                textData.append(line)
-            else:
-                currentGameId = line[0]
-                parsedData[currentGameId] = GameData(currentGameId, [dataLine])
-
-print(parsedData[parsedData.keys()[0]].data)
+if sys.argv[1] == "--help":
+    print("parseNBAData.py [season] [gameID] [player]")
+    print("season: season of txt file")
+else: 
+    textData = []
+    parsedData = {}
+    with open('./'+ sys.argv[1] +'.txt', 'rb') as tsvin:
+        tsvin = csv.reader(tsvin, delimiter = "\t")
+        currentGameId = ""
+        for i, line in enumerate(tsvin):
+            if i > 0:
+                timeInSeconds = 48 * 60 - get_sec(line[2])
+                dataLine = line[2:4]
+                dataLine[0] = timeInSeconds
+                if currentGameId == "":
+                    currentGameId = line[0]
+                    parsedData[currentGameId] = GameData(currentGameId, [dataLine])
+                elif currentGameId == line[0]:
+                    parsedData[currentGameId].append(dataLine)
+                    textData.append(line)
+                else:
+                    currentGameId = line[0]
+                    parsedData[currentGameId] = GameData(currentGameId, [dataLine])
+    print(parsedData[parsedData.keys()[0]].data)
