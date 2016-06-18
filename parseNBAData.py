0a2152eb984ab4a4e20f13c499ecf84de481c470
joeyism <sham.joey@gmail.com>
2016-06-18 01:22:26 -0400

    Output based on user input

--- parseNBAData.py
+++ parseNBAData.py
@@ -11,6 +11,19 @@ class GameData:
     def append(self, data):
         self.data.append(data)
 
+class userGameData:
+    data = []
+    def __init__(self, GameID, playerID):
+        self.GameID = GameID
+        self.playerID = playerID
+    def append(self, data):
+        self.data.append(data)
+    def __str__(self):
+        ret = ""
+        for event in self.data:
+            ret += str(event[0]) + " " + event[1] + "\n"
+        return ret
+
 def get_sec(s):
     l = s.split(':')
     return int(l[0]) * 3600 + int(l[1]) * 60 + int(l[2])
@@ -19,8 +32,11 @@ if sys.argv[1] == "--help":
     print("parseNBAData.py [season] [gameID] [player]")
     print("season: season of txt file")
 else: 
+    userInputGameId = sys.argv[2]
+    player = sys.argv[3]
     textData = []
     parsedData = {}
+    thisUserGameData = userGameData(userInputGameId, player)
     with open('./'+ sys.argv[1] +'.txt', 'rb') as tsvin:
         tsvin = csv.reader(tsvin, delimiter = "\t")
         currentGameId = ""
@@ -38,4 +54,7 @@ else:
                 else:
                     currentGameId = line[0]
                     parsedData[currentGameId] = GameData(currentGameId, [dataLine])
-    print(parsedData[parsedData.keys()[0]].data)
+                
+                if player in dataLine[1]:
+                    thisUserGameData.append(dataLine)
+    print(thisUserGameData)
